4baad10aedcc6eb2a61511d5da4ec5ef12cf00f4
Luis Eduardo M <luiseduardomc019@gmail.com>
2022-06-05 18:39:31 -0700

    two sum

---
+++ Go/TwoSum/twosum.go
@@ -0,0 +1,16 @@
+package main
+
+// Time complexity == O(n²)
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == target-nums[i] {
+				r := []int{i, j}
+				return r
+			}
+		}
+	}
+	return nil
+}
+
+// Optimization
